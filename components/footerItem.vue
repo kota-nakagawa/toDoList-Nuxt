87122e832da5deb8f671428e9e70589286381cb5
nyanko-kota <mail@nyanko-kota.com>
2019-10-25 15:15:07 +0900

    [fix] footerの構造を変更

--- components/footerItem.vue
+++ components/footerItem.vue
@@ -9,15 +9,19 @@
 
 <style scoped>
 .footer-top {
-  padding-top: 50px;
+  padding-top: 75px;
 }
 .footer {
   position: fixed;
   z-index: 10;
   bottom: 0;
-  border-top: solid 1px #E0E0E0;
+  height: 75px;
   width: 100%;
-  background: #FFFFFF;
   max-width: 600px;
+  background: #FFFFFF;
+  border-top: solid 1px #E0E0E0;
+}
+.footer p {
+  font-size: 20px;
 }
 </style>
