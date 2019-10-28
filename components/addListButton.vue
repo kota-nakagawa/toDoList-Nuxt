33cb15e7b249a9873be7ebffd444acd1c051b756
nyanko-kota <mail@nyanko-kota.com>
2019-10-28 10:07:51 +0900

    [add] ボタンのCSSを記載

--- components/addListButton.vue
+++ components/addListButton.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex-container">
     <div class="fix-position">
-      <div class="button" />
+      <div class="button">
+        <span class="hamburger-item1" />
+        <span class="hamburger-item2" />
+      </div>
     </div>
   </div>
 </template>
@@ -17,9 +20,35 @@
 .button {
   height: 55px;
   width: 55px;
-  background-color: blue;
+  background-color: rgb(29, 161, 242);
   z-index: 15;
   bottom: 120px;
   position: fixed;
+  box-shadow: 0 3px 5px -1px rgba(0,0,0,.2);
+  border-radius: 50%;
+}
+.hamburger-item1 {
+  transform: translateY(0) rotate(0deg);
+  width: 30px;
+  height: 4px;
+  background-color: #f5f7fa;
+  position: absolute;
+  display: block;
+  left: 0;
+  right: 0;
+  margin: auto;
+  top: 24px;
+}
+.hamburger-item2 {
+  transform: translateY(0) rotate(90deg);
+  width: 30px;
+  height: 4px;
+  background-color: #f5f7fa;
+  position: absolute;
+  display: block;
+  left: 0;
+  right: 0;
+  margin: auto;
+  top: 25px;
 }
 </style>
--- components/headerItem.vue
+++ components/headerItem.vue
@@ -4,7 +4,6 @@
       <div class="header-title">
         <p>{{ title }}</p>
       </div>
-      <div class="add-button" />
     </div>
   </div>
 </template>
@@ -28,15 +27,8 @@ export default {
 .header-title p {
   margin: 0;
   padding-top: 30px;
-  padding-left: 15px;
+  padding-left: 25px;
   font-size: 30px;
   font-family: 'Hiragino Maru Gothic ProN'
-}
-.add-button {
-  margin-top: 30px;
-  margin-right: 15px;
-  height: 45px;
-  width: 45px;
-  background-color: blue;
 }
 </style>
