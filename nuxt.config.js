a18fdf3a3d96486a508bc57043ff53bed7c2192e
nyanko-kota <mail@nyanko-kota.com>
2019-10-28 16:34:46 +0900

    [add] デフォルト言語を日本語に設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,9 +5,13 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'toDoList',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
+      { name: 'description', content: 'シンプルなtoDoList' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
