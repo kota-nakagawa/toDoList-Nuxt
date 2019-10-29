194e17aac6a477f8fae9d36ac1484623c03141e5
nyanko-kota <mail@nyanko-kota.com>
2019-10-29 11:30:48 +0900

    [add] vuexの値をlist.vueにマウント　

--- pages/list.vue
+++ pages/list.vue
@@ -15,19 +15,23 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import listItem from '@/components/listItem'
 import addListButton from '@/components/addListButton'
 export default {
+  components: {
+    listItem,
+    addListButton
+  },
+  computed: {
+    ...mapState(['tasks'])
+  },
   transition (to, from) {
     if (!from) { return }
     const map = { index: 0, list: 1, settings: 2 }
     return {
       name: map[to.name] < map[from.name] ? 'slide-right' : 'slide-left'
     }
-  },
-  components: {
-    listItem,
-    addListButton
   },
   created () {
     this.$store.commit('setPageTitle', this.$metaInfo.title)
